87557af76df7b0d99db3cc4743d979d2860f399d
cuzik <cuzikjack@gmail.com>
2019-03-05 11:48:53 -0300

    clean seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 account1 = Account.create(name: 'Empresa 01', document: '0001')
 account2 = Account.create(name: 'Empresa 02', document: '0002')
 
